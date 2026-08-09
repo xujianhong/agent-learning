970537ac18f61d7132056feb298430182df7ef64
jianhongxu <xjh571152503@icloud.com>
2026-08-09 23:35:54 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import time
+from ollama import chat
+
+# start = time.perf_counter()
+
+conversation = [
+	{
+		"role": "system",
+		"content": """
+You are a Python programming tutor.
+
+Your job is to help the user learn Python and AI agent development.
+
+Rules:
+- Explain concepts clearly.
+- Assume the user understands basic programming.
+- Give examples when useful.
+- Don't just give the answer; explain why it works."""
+
+}]
+
+while True:
+	user_input = input("You: ")
+
+	if user_input.lower() in {"exit","quit"}:
+		break
+	conversation.append({
+		"role": "user",
+		"content": user_input,
+	})
+	start = time.perf_counter()
+	print(f"[{time.perf_counter() - start:.2f}s] Starting qwen ")
+	response = chat(
+		model = "qwen:latest",
+		messages = conversation,
+	)
+
+	print(f"[{time.perf_counter() - start:.2f}s] Finished qwen")
+	answer = response.message.content
+
+	print(f"Assistant: {answer}")
+
+	print(f"[{time.perf_counter() - start:.2f}s] Finished printing answer")
+
+	conversation.append({
+		"role": "assistant",
+		"content": answer,
+		})
+# response = chat(
+# 	model = "qwen:latest",
+# 	messages=[
+# 		{
+# 	 	"role": "user",
+# 		"content": "Explain what an AI agent is in one sentence."
+# 		}
+# 	],
+# )
+
+# end = time.time()
+
+# print(response.message.content)
+# print(f"Execution time: {end - start:.2f} seconds")
